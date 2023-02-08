e476530a8d42026549013722bcda40ce014efa87
Hana Lee <leehana@alumni.stanford.edu>
2023-02-08 15:49:50 -0800

    add ability to delete scenario configurations

--- src/interface/src/app/plan/plan-summary/scenario-configurations/scenario-configurations.component.spec.ts
+++ src/interface/src/app/plan/plan-summary/scenario-configurations/scenario-configurations.component.spec.ts
@@ -1,24 +1,35 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ProjectConfig } from 'src/app/types';
 import { BehaviorSubject, of } from 'rxjs';
 import { MapService, PlanService } from 'src/app/services';
-import { MaterialModule } from 'src/app/material/material.module';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/material/material.module';
+import { ProjectConfig } from 'src/app/types';
 
 import { ScenarioConfigurationsComponent } from './scenario-configurations.component';
 
 describe('ScenarioConfigurationsComponent', () => {
   let component: ScenarioConfigurationsComponent;
   let fixture: ComponentFixture<ScenarioConfigurationsComponent>;
+  let loader: HarnessLoader;
   let fakeMapService: MapService;
   let fakePlanService: PlanService;
 
-  const fakeConfig: ProjectConfig = {
-    id: 1,
-    priorities: ['fake_priority']
-  };
+  let fakeConfigs: ProjectConfig[];
 
   beforeEach(async () => {
+    fakeConfigs = [
+      {
+        id: 1,
+        priorities: ['fake_priority']
+      },
+      {
+        id: 2,
+      },
+    ];
+
     let nameMap = new Map<string, string>();
     nameMap.set('fake_priority', 'fake_display_priority');
 
@@ -32,14 +43,15 @@ describe('ScenarioConfigurationsComponent', () => {
     fakePlanService = jasmine.createSpyObj<PlanService>(
       'PlanService',
       {
-        getProjects: of([fakeConfig])
+        deleteProjects: of([1]),
+        getProjects: of(fakeConfigs),
       },
       {}
     );
 
     await TestBed.configureTestingModule({
-      imports: [ MaterialModule ],
-      declarations: [ ScenarioConfigurationsComponent ],
+      imports: [MaterialModule, NoopAnimationsModule],
+      declarations: [ScenarioConfigurationsComponent],
       providers: [
         {
           provide: MapService, useValue: fakeMapService
@@ -53,6 +65,7 @@ describe('ScenarioConfigurationsComponent', () => {
 
     fixture = TestBed.createComponent(ScenarioConfigurationsComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
@@ -62,10 +75,40 @@ describe('ScenarioConfigurationsComponent', () => {
 
   it('should get a list of scenario configs', () => {
     expect(fakePlanService.getProjects).toHaveBeenCalled();
-    expect(component.configurations).toEqual([fakeConfig]);
+    expect(component.configurations).toEqual(fakeConfigs);
+  });
+
+  it('should show delete button if at least one config is selected', () => {
+    expect(component.showDeleteButton()).toBeFalse();
+
+    component.configurations[0].selected = true;
+
+    expect(component.showDeleteButton()).toBeTrue();
+  });
+
+  it('should show continue button if exactly one config is selected', () => {
+    expect(component.showContinueButton()).toBeFalse();
+
+    component.configurations[0].selected = true;
+
+    expect(component.showContinueButton()).toBeTrue();
+
+    component.configurations[1].selected = true;
+
+    expect(component.showContinueButton()).toBeFalse();
+  });
+
+  it('should call service when delete button is clicked', async () => {
+    component.configurations[0].selected = true;
+    component.configurations[1].selected = true;
+    let deleteButton = await loader.getHarness(MatButtonHarness);
+
+    await deleteButton.click();
+
+    expect(fakePlanService.deleteProjects).toHaveBeenCalledOnceWith([1, 2]);
   });
 
   it('should map priorities to display names', () => {
-    expect(component.displayPriorities(fakeConfig.priorities!)).toEqual(['fake_display_priority']);
+    expect(component.displayPriorities(fakeConfigs[0].priorities!)).toEqual(['fake_display_priority']);
   });
 });
--- src/interface/src/app/plan/plan-summary/scenario-configurations/scenario-configurations.component.ts
+++ src/interface/src/app/plan/plan-summary/scenario-configurations/scenario-configurations.component.ts
@@ -1,7 +1,13 @@
 import { MapService } from './../../../services/map.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, Input, OnInit } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { PlanService } from 'src/app/services';
 import { Plan, ProjectConfig } from 'src/app/types';
+
+interface ProjectConfigRow extends ProjectConfig {
+  selected?: boolean;
+}
 
 @Component({
   selector: 'app-scenario-configurations',
@@ -10,7 +16,7 @@ import { Plan, ProjectConfig } from 'src/app/types';
 })
 export class ScenarioConfigurationsComponent implements OnInit {
   @Input() plan: Plan | null = null;
-  configurations: ProjectConfig[] = [];
+  configurations: ProjectConfigRow[] = [];
   displayedColumns: string[] = [
     'select',
     'createdTimestamp',
@@ -21,10 +27,15 @@ export class ScenarioConfigurationsComponent implements OnInit {
 
   constructor(
     private mapService: MapService,
-    private planService: PlanService
+    private planService: PlanService,
+    private snackbar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
+    this.fetchProjects();
+  }
+
+  fetchProjects(): void {
     this.planService.getProjects(this.plan?.id!).subscribe((result) => {
       this.configurations = result;
     });
@@ -37,5 +48,28 @@ export class ScenarioConfigurationsComponent implements OnInit {
         if (nameMap.has(priority)) return nameMap.get(priority)!;
         return priority;
       });
+  }
+
+  selectAllConfigs(event: MatCheckboxChange): void {
+    this.configurations.forEach((config) => (config.selected = event.checked));
+  }
+
+  showDeleteButton(): boolean {
+    return this.configurations.filter((config) => config.selected).length > 0;
+  }
+
+  showContinueButton(): boolean {
+    return this.configurations.filter((config) => config.selected).length === 1;
+  }
+
+  deleteSelectedConfigs(): void {
+    this.planService.deleteProjects(
+      this.configurations
+        .filter((config) => config.selected)
+        .map((config) => config.id)
+    ).subscribe((deletedIds) => {
+      this.snackbar.open(`Deleted ${deletedIds.length} configuration${deletedIds.length > 1 ? 's' : ''}`);
+      this.fetchProjects();
+    });
   }
 }
